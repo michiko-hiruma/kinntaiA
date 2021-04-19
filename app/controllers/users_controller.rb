136b382090f9765c0adec2a29f82f51d0bad1cbf
比留間 <hiruma7878@gmail.com>
2021-04-19 15:13:46 +0000

    10.4章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,13 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
   end
 
   def new
@@ -31,15 +30,14 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def update_basic_info
-   if @user.update_attributes(basic_info_params)
-     flash[:success] = "#{@user.name}の基本情報を更新しました。"
-   else
-      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
-   end
-   redirect_to users_url
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit      
+    end
   end
-end
 
   def destroy
     @user.destroy
@@ -51,12 +49,12 @@ end
   end
 
   def update_basic_info
-   if @user.update_attributes(basic_info_params)
-      # 更新成功時の処理
-   else
-     # 更新失敗時の処理
-   end
-  redirect_to users_url
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
 
   private
@@ -64,7 +62,7 @@ end
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-    
+
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
@@ -94,4 +92,4 @@ end
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
-  
+end

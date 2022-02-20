e0557d8d2c14af75f24ea4773e866dde9192311a
EC2 Default User <ec2-user@ip-172-31-2-118.ap-northeast-1.compute.internal>
2022-02-20 03:53:03 +0000

    勤怠表示ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
 
   def index
+    debugger
+
     # @users = User.paginate(page: params[:page])
     @users = User.where('name LIKE(?)', "%#{params[:name]}%").paginate(page: params[:page], per_page: 20)
   end

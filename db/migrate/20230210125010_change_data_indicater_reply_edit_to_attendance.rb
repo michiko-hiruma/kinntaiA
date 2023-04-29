2f5fcd2e5be4f6d71e897ccb401ddb8b37fcba53
EC2 Default User <ec2-user@ip-172-31-46-185.ap-northeast-1.compute.internal>
2023-04-29 01:56:06 +0000

    Update Gemfile for Heroku6

--- db/migrate/20230210125010_change_data_indicater_reply_edit_to_attendance.rb
+++ db/migrate/20230210125010_change_data_indicater_reply_edit_to_attendance.rb
@@ -1,5 +1,5 @@
 class ChangeDataIndicaterReplyEditToAttendance < ActiveRecord::Migration[6.1]
   def change
-   change_column :attendances, :indicater_reply_edit, :string
+   change_column :attendances, :indicater_reply_edit, :integer
   end
 end
--- db/migrate/20230221015647_change_datatype_indicater_reply_edit_of_attendance.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeIndicaterReplyEditOfAttendance < ActiveRecord::Migration[6.1]
-  def change
-    change_column :attendances, :indicater_reply_edit, :integer
-  end
-end

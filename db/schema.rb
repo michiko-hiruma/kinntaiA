62045aa2acebe47d54741fb2c60c9680f854ecfc
EC2 Default User <ec2-user@ip-172-31-46-185.ap-northeast-1.compute.internal>
2023-04-29 00:38:23 +0000

    Update Gemfile for Heroku3

--- db/schema.rb
+++ db/schema.rb
@@ -89,15 +89,15 @@ ActiveRecord::Schema.define(version: 2023_03_25_011838) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2023-04-23 23:00:00"
-    t.datetime "work_time", default: "2023-04-23 22:30:00"
+    t.datetime "basic_time", default: "2023-04-28 23:00:00"
+    t.datetime "work_time", default: "2023-04-28 22:30:00"
     t.boolean "superior", default: false
     t.string "affiliation"
     t.integer "employee_number"
     t.string "uid"
-    t.datetime "designated_work_start_time", default: "2023-04-24 00:00:00"
-    t.datetime "designated_work_end_time", default: "2023-04-24 09:00:00"
-    t.datetime "basic_work_time", default: "2023-04-23 23:00:00"
+    t.datetime "designated_work_start_time", default: "2023-04-29 00:00:00"
+    t.datetime "designated_work_end_time", default: "2023-04-29 09:00:00"
+    t.datetime "basic_work_time", default: "2023-04-28 23:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

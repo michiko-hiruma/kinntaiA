ed7b6cfd858cf06683e5b92295b506ad014561fc
EC2 Default User <ec2-user@ip-172-31-46-185.ap-northeast-1.compute.internal>
2023-04-29 01:47:07 +0000

    Update Gemfile for Heroku5

--- db/migrate/20230116031244_add_started_edit_at_to_attendances.rb
+++ db/migrate/20230116031244_add_started_edit_at_to_attendances.rb
@@ -1,5 +1,5 @@
 class AddStartedEditAtToAttendances < ActiveRecord::Migration[6.1]
   def change
-    add_column :attendances, :started_edit_at, :string
+    add_column :attendances, :started_edit_at, :datetime
   end
 end
--- db/migrate/20230116113234_change_datatype_started_edit_at_of_attendances.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDatatypeStartedEditAtOfAttendances < ActiveRecord::Migration[6.1]
-  def change
-    change_column :attendances, :started_edit_at, :datetime
-    
-  end
-end
